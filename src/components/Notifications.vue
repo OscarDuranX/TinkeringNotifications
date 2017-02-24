6c0fc45b9ec4d742a0a45a51419af18ba17709bb
Oscar Duran <xoscarduranx@gmail.com>
2017-02-24 17:09:12 +0100

    Initial version

---
+++ src/components/Notifications.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ msg }}
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+  export default {
+  name: 'notifications',
+  data () {
+    return {
+      msg: 'TODOS!!!-!'
+    }
+  }
+}
+
+</script>
