7a809afc26a7d7868b2f6dda5f4cbe750b9b224f
Oscar Duran <xoscarduranx@gmail.com>
2017-02-25 17:18:33 +0100

    arreglat

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <input type="button" value="show notification" @click="showNotification">
         {{ msg }}
     </div>
 </template>
@@ -7,12 +8,29 @@
 
 </style>
 <script>
-  export default {
+export default {
   name: 'notifications',
   data () {
     return {
       msg: 'TODOS!!!-!'
     }
+  },
+  methods: {
+    showNotification: function () {
+      console.log('prova')
+      if (Notification.permission === 'granted') {
+        var notification = new Notification('Hi there!')
+        notification
+      }
+    }
+  },
+  created () {
+    if (!('Notification' in window)) {
+      alert('This browser does not support system notifications')
+    }
+    Notification.requestPermission().then(function (result) {
+      console.log(result)
+    })
   }
 }
 
